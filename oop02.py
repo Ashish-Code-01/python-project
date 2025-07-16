dc3964384b77c54574d53dda1be3f918e97c5046
Ashish-Code-01 <tanujgaming87@gmail.com>
2025-07-16 18:09:42 +0530

    add the notes of constructor

--- oop02.py
+++ oop02.py
@@ -1,6 +1,11 @@
 # This is the class person
 class Person:
-    # This is the class Constructor it will take argument. when the class is created , like this  Person("Ashish", "Founder", "50M$", 19)
+    # The __init__ function is called a constructor in Python.
+    # It is automatically called when a new object of the class is created.
+    # Its main job is to initialize (set up) the object's attributes with values provided as arguments.
+    # Syntax: def __init__(self, ...):
+    # 'self' refers to the current object being created.
+    # Example usage: a = Person("Ashish", "Founder", "50M$", 19)
     def __init__(self, name, occupation, networth, age):
         self.name = name
         self.occupation = occupation
@@ -14,6 +19,18 @@ class Person:
             f"Hii, My name is {self.name}, He is {self.occupation} and his networth is {self.netWorth}, he is {self.age} year old"
         )
 
+
+# Notes on __init__ function:
+"""
+- __init__ is a special method in Python classes, known as the constructor.
+- It is used to initialize the attributes of a class when an object is created.
+- The first parameter is always 'self', which refers to the instance being created.
+- You can pass any number of arguments to __init__, which are used to set up the object's state.
+- Example:
+    a = Person("Ashish", "Founder", "50M$", 19)
+    This calls __init__ and sets the attributes for the object 'a'.
+- Without __init__, you would have to set each attribute manually after creating the object.
+"""
 
 # create a object of class Person
 a = Person("Ashish", "Founder", "50M$", 19)
